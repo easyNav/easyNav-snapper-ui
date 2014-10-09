2d77f5251e3a9180a7b40e7f6f748a66a7d117f1
Joel Tong <me@joeltong.org>
2014-10-09 15:47:45 +0800

    allowed prediction to return value

--- easyNav_snapper_ui/snapperWidget.py
+++ easyNav_snapper_ui/snapperWidget.py
@@ -9,7 +9,7 @@
 # Copyright (c) 2014 Joel Tong me@joeltong.org
 
 
-from gi.repository import Gtk 
+from gi.repository import Gtk, GObject
 import logging
 import smokesignal
 
@@ -17,8 +17,12 @@ from easyNav_snapper import Snapper
 from SerialDaemon import SerialDaemon
 
 
+GObject.threads_init()
+
+
 class SnapperWidget(Gtk.Window):
     def __init__(self):
+
         filename = "snapTemplate.glade"
         self.builder = Gtk.Builder()
         self.builder.add_from_file(filename)
@@ -29,11 +33,11 @@ class SnapperWidget(Gtk.Window):
         self.snapper = Snapper()
         self.saveFilepath = None
 
-
         ## For SerialDaemon stuff
         sd = self.sd = SerialDaemon()
         self._attachHandlers()
         sd.start()
+
 
 
     def _attachHandlers(self):
@@ -45,6 +49,11 @@ class SnapperWidget(Gtk.Window):
             """ Event callback for serial data 
             """
             logging.info('Serial Daemon: New Data!')
+            self.builder.get_object('bField_x').set_text(str(x))
+            self.builder.get_object('bField_y').set_text(str(y))
+            self.builder.get_object('bField_z').set_text(str(z))
+            self.builder.get_object('bField_norm').set_text(str(intensity))
+
             print (x,y,z,intensity)
 
 
@@ -119,12 +128,48 @@ class SnapperWidget(Gtk.Window):
         chooser_dialog.destroy()
 
 
+    def on_btnExport_activate(self, args):
+        """ Export to SVM Light format
+        """
+        print 'selected menu'
+        chooser_dialog = Gtk.FileChooserDialog(title='Load file..'
+            , action=Gtk.FileChooserAction.SAVE
+            , buttons=["Export to..", Gtk.ResponseType.OK, "Cancel", Gtk.ResponseType.CANCEL])
+
+        Gtk.FileChooser.set_do_overwrite_confirmation(chooser_dialog, True)
+
+        pyFilter = Gtk.FileFilter()
+        pyFilter.add_mime_type('text/x-python')
+        pyFilter.add_pattern('*.py')
+        pyFilter.set_name('Python files')
+
+
+        response = chooser_dialog.run() 
+
+        if response == Gtk.ResponseType.OK:
+            filename = chooser_dialog.get_filename()
+
+            # Save information from snapper 
+            self.snapper.export(filename)
+
+            self.builder.get_object('statusbar1').push(0, 'File %s loaded.' % filename)
+            print chooser_dialog.get_filename(), 'selected'
+
+        elif response == Gtk.ResponseType.CANCEL:
+            print 'Closed, no files selected'
+        chooser_dialog.destroy()
+
+
+
+
     def on_btnAdd_clicked(self, args):
+        """ Add a new Entry
+        """
         bFieldX = float(self.builder.get_object('bField_x').get_text())
         bFieldY = float(self.builder.get_object('bField_y').get_text())
         bFieldZ = float(self.builder.get_object('bField_z').get_text())
+        bFieldMagnitude = float(self.builder.get_object('bField_norm').get_text())
         locId = int(self.builder.get_object('locId').get_text())
-        bFieldMagnitude = (bFieldX**2 + bFieldY**2 + bFieldZ**2) ** .5
 
         print bFieldMagnitude, locId
 
@@ -141,6 +186,24 @@ class SnapperWidget(Gtk.Window):
                                                         locId,
                                                         bFieldMagnitude))
 
+
+
+    def on_btnTrain_clicked(self, args):
+        """ Train button 
+        """
+        neighbors = int(self.builder.get_object('entryNeighbors').get_text())
+        self.snapper.train(neighbors)
+        self.builder.get_object('statusbar1').push(0, 'Trained model.')
+
+
+    def on_btnPredict_clicked(self, args):
+        """ Predict button 
+        """
+        bFieldMagnitude = float(self.builder.get_object('bField_norm').get_text())
+        prediction = self.snapper.predict({
+            'bField': bFieldMagnitude
+        })
+        self.builder.get_object('statusbar1').push(0, 'The prediction is: %s' % prediction)
 
 
 
